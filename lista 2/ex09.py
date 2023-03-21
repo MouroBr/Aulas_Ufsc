5a85234ed1756746216f4ef9c778ef689413fe3b
MouroBr <brizolla.bruno@gmail.com>
2023-03-20 21:13:12 -0300

    Lista de exercicios 02

---
+++ lista 2/ex09.py
@@ -0,0 +1,8 @@
+A, B, C = [float(x) for x in input().split()]
+
+if A == 0 and (B)**2 < (4 * A * C):
+    print('Impossivel calcular')
+
+else:
+    print(f'R1 = { ((-B) + ((B)**2 - 4 * A * C)**0.5) / (2 * A):.5}')
+    print(f'R2 = {((-B) - ((B)**2 - 4 * A * C)**0.5) / (2 * A):.5}')
