ff0b81296e5bf9cfeb084fe2cd775766c68a31d1
MouroBr <brizolla.bruno@gmail.com>
2023-03-20 21:17:21 -0300

    Lista de exercicios 02

--- lista 2/ex09.py
+++ lista 2/ex09.py
@@ -4,5 +4,5 @@ if A == 0 and (B)**2 < (4 * A * C):
     print('Impossivel calcular')
 
 else:
-    print(f'R1 = { ((-B) + ((B)**2 - 4 * A * C)**0.5) / (2 * A):.5}')
+    print(f'R1 = {((-B) + ((B)**2 - 4 * A * C)**0.5) / (2 * A):.5}')
     print(f'R2 = {((-B) - ((B)**2 - 4 * A * C)**0.5) / (2 * A):.5}')
